19c46d089e985d0a684ee98f9e63afeefa78f20c
Benjamin Croker <benjamin.croker@gmail.com>
2014-08-14 09:37:28 +1000

    add public host for running app

--- qrequest.py
+++ qrequest.py
@@ -3,4 +3,4 @@ import database as db
 
 if __name__ == '__main__':
 
-    webserver.app.run(port=db.SETTINGS['website_port_number'], debug=False)
+    webserver.app.run(host='0.0.0.0', port=db.SETTINGS['website_port_number'], debug=False)
