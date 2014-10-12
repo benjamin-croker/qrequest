77ae1db3a9f2363ce5d6b1fdd2acb2794d94d03f
Benjamin Croker <benjamin.croker@gmail.com>
2014-10-12 15:18:35 +1000

    turn debug mode off

--- qrequest.py
+++ qrequest.py
@@ -1,4 +1,4 @@
 import webserver
 
 if __name__ == '__main__':
-    webserver.app.run(host='0.0.0.0', port=webserver.settings['website_port_number'], debug=True)
+    webserver.app.run(host='0.0.0.0', port=webserver.settings['website_port_number'], debug=False)
